f9e54b30751d0ce8794283cc2a75e93222e6b204
Dominik B <domburkart@gmail.com>
2022-03-01 15:31:34 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff'
+    },
+    fontFamily: {
+      'mono': ['"IBM Plex Mono"'],
+      'display': ['Poppins']
+    },
+    extend: { },
+  },
+  plugins: [""],
+}
